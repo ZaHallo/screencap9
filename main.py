036085b3064c027e04a0133e2f3cb343ddff705f
ZaHallo <earnold2026@sau52.org>
2023-11-08 11:15:55 -0500

    yes

--- main.py
+++ main.py
@@ -2,16 +2,39 @@ import numpy as np
 import cv2
 from mss import mss
 from PIL import Image
-# from screeninfo import get_monitors
+from requests import request
+import time
+from tkinter import *
+from tkinter.ttk import *
 
-bounding_box = {'top': 0, 'left': 0, 'width': 400, 'height': 300}
-# m1 = get_monitors()[0]
+root = Tk()
+root.title = "carryover9"
+root.geometry('200x150')
+varStatus = StringVar()
+varEntered = StringVar()
+txtStatus = Label(root, textvariable=varStatus)
+txtboxSharename = Text(root)
+
+txtStatus.pack(side="top")
+
+def status(txt):
+    varStatus.set(txt)
+
+BASEURL = "https://carryover.nerdakus.repl.co/"
+status("pinging server...")
+request("GET", BASEURL)
+txtboxSharename.pack(side="top")
+status("enter sharename")
+varEntered.wai
+res = request("POST", BASEURL+"reg/"+name)
+status(res.text)
 
 sct = mss()
 while True:
+    root.update()
     sct_img = sct.grab(sct.monitors[0])
     matlik = np.array(sct_img)
-    print(len(matlik))
+    # print(len(matlik))
     # matlik = cv2.resize(matlik, dsize=(int(m1.width/16), int(m1.height/16)))
     res = cv2.resize(matlik, None, fx=0.5, fy=0.5, interpolation=cv2.INTER_NEAREST)
     cv2.imshow('screen', res)
